542cf3fc03ed20a662581f02c77bfb9bfb30735d
MolinariP <136937416+MolinariP@users.noreply.github.com>
2025-08-20 22:21:14 -0300

    Update TP2Ejercicio4.java

--- Ejercicio 4/TP2Ejercicio4.java
+++ Ejercicio 4/TP2Ejercicio4.java
@@ -16,19 +16,7 @@ public class TP2Ejercicio4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-           // 4. Calculadora de Descuento
-       /* System.out.print("Ingrese el precio del producto: ");
-        double precio = scan.nextDouble();
-        System.out.print("Ingrese la categoría (A, B o C): ");
-        char cat = scan.next().toUpperCase().charAt(0);
-        double descuento = 0;
-        if (cat == 'A') descuento = 0.10;
-        else if (cat == 'B') descuento = 0.15;
-        else if (cat == 'C') descuento = 0.20;
-        double precioFinal = precio - (precio * descuento);
-        System.out.println("Descuento aplicado: " + (descuento * 100) + "%");
-        System.out.println("Precio final: " + precioFinal);*/
+       
         
         double precioOriginal, descuento = 0, precioFinal;
         char categoria;
